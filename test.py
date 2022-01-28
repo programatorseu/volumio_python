9b25354f2e0de67ca3ed8c21cb9192918832e927
Piotr Sadowski <programatorseu@gmail.com>
2022-01-28 22:01:24 +0100

    Modified lines of python testing

--- test.py
+++ test.py
@@ -1,37 +1,21 @@
-def break_words(stuff):
-    """THis func will break up words for us """
-    words = stuff.split(' ')
-    return words
+from sys import exit
 
-def sort_words(words):
-    """Sort the words"""
-    return sorted(words)
+def gold_room():
+    print("room full of gold . How much do you take?")
+    choice = input("> ")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number")    
 
+def bear_room():
+    print("There is a bear there")
+    print("The bear has a bunch of honey") 
+    print("The fat baer is in front of another door")
+    print("How are you going to move the bear?")
+    bear_moved = False
 
-def print_first_word(words):
-    """Print the first word after popping it off"""
-    word = words.pop(0)
-    print(word)
-
-def print_last_word(words):
-    """Print the last word after popping it off"""
-    word = words.pop(-1)
-    print(word)
-
-def sort_sentence(sentence):
-    """Takes in a full  and returns the sorted words"""
-    words = break_words(sentence)
-    return sort_words(words)
-
-def print_first_and_last(sentence):
-    """Prints the first and last words of the sentence"""
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
-
-def print_first_and_last_sorted(sentence):
-    """Sorts words then prints the first and last one"""
-    words = sort_sentence(sentence)
-    print_first_word(words)
-    print_last_word(words)
-print('EOF')        
+    while True:
+        choice = input("> ")
+        if choice == "take honey":
+            dead("The Bear looks at you then slaps your face oof")
